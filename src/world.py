a06534e8f0653612ecb10c420f7f4ad2906ff3a3
dakri7 <daniel.krimm@stud.tu-darmstadt.de>
2024-03-18 15:52:34 +0100

    Mathematical outline of trace_particle, Some thoughts on the poisson solver

--- src/world.py
+++ src/world.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import Tuple
 import numpy as np
 
@@ -6,8 +7,8 @@ class World2D:
     def __init__(self, size: Tuple[float, float], shape: Tuple[int, int], viscosity: float, diffusion: float,
                  dissipation_rate: float):
         # World parameters
-        self.size = size
-        self.shape = shape
+        self.size = size                    # L
+        self.shape = shape                  # N
 
         # Field
         self.velocity0 = np.zeros(shape)    # U0
@@ -22,6 +23,10 @@ class World2D:
 
         self.force = 0  # TODO for future use
         self.force_source = (0, 0)  #TODO for future use
+
+        self.voxel_size = [0] * len(size)   # D
+        for i in range(len(size)):
+            self.voxel_size[i] = size[i] / shape[i]
 
     def apply_force(self, position: Tuple[int, int], force: Tuple[float, float]):
         pass
@@ -45,9 +50,10 @@ class World2D:
         self.diffuse_field(self.density0, self.density1, self.diffusion, self.dt)
         self.dissipate_field(self.density0, self.density1, self.dissipation_rate, self.dt)
 
-    def add_force_velocity(self, velocity0, force, dt: float): - None
+    def add_force_velocity(self, velocity0, force, dt: float) -> None:
         pass
 
+    #Appendix A
     def transport_velocity(self, velocity0, velocity1, velocity_total, dt: float) -> None:
         pass
 
@@ -60,8 +66,12 @@ class World2D:
     def add_force_field(self, density0, source, dt: float) -> None:
         return density0 + source * dt
 
+    # p 125 transport function
     def transport_field(self, density0, density1, velocity, dt: float) -> None:
-        pass
+        for i, j in itertools.product(range(self.shape[0]), range(self.shape[1])):
+            x = (i+0.5, j+0.5) #FIXME: Original formula is multiplied by "D". What is D?
+            x_prev = self.trace_particle(x, velocity, dt)
+            density1[i, j] = density0[x_prev]   # FIXME Original formula interpolates linearly? How, what and why?
 
     def diffuse_field(self, density0, density1, diffusion: float, dt: float) -> None:
         pass
@@ -69,8 +79,25 @@ class World2D:
     def dissipate_field(self, density0, density1, dissipation_rate: float, dt: float) -> None:
         pass
 
-    def trace_particle(self, pos: (int, int), velocity, dt, steps=100) -> (int, int):
+    # Calculates the position of a particle in the field at the last timestep. Used for advection(transport)
+    # p. 125 bottom right
+    def trace_particle(self, pos: (int, int), velocity, dt, steps=10) -> (int, int):
+        h = -dt / steps     # Note that we use -dt to traverse the fild in the opposite direction
+        curr_pos = pos
+        # FIXME: Correct implementation of tuple addition/multiplication (pairwise)
+        # FIXME: Find out how to handle float indices (maybe interpolate between neighboring cells?, Maybe round to next int?)
+        # FIXME: velocity is a tuple of arrays
         for i in range(steps):
-            k0 = velocity[pos]
-            k1_x = pos + 0.5 * k0
+            k1 = h * velocity[curr_pos]
+            k2 = h * velocity[curr_pos + k1/2]
+            curr_pos = curr_pos + k2
+
+    # Poisson 2d cartesian differential equation:
+    # δ^2*u/δ^2x + δ^2*u/δ^2x = f(x, y)
+    #
+    # Finite difference equation TODO: validate
+    # δ^2*u/δ^2x = (u[i+1, j] - 2u[i, j] + u[i-1, j]) / dx^2
+    # δ^2*u/δ^2y = (u[i, j+1] - 2u[i, j] + u[i, j-1]) / dy^2
+    def cartesian_poisson_2d(self, u, pos:(int, int)):
         pass
+
