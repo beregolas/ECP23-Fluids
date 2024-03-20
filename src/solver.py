8db905a85eed02036fc4cd2a1ac129d517b2c054
julius <juliuskilger@posteo.de>
2024-03-20 20:28:54 +0100

    new branch

--- src/solver.py
+++ src/solver.py
@@ -1,16 +1,16 @@
 import numpy as np
 import imageio
 
-from world import World2D
+# from world import World2D
 
 if __name__ == "__main__":
+    # TODO: get input image
 
-    #TODO: get input image
-
-    world = World2D((10, 10), (10, 10), 0.0005, 0.01, 0.01)
+    # world = World2D((10, 10), (10, 10), 0.0005, 0.01, 0.01)
 
     for i in range(30):
-        world.step(0.1)
+        # world.step(0.1)
+        pass
 
     # TODO: output image with imageio
     # maybe one image per step for an animation(gif)
