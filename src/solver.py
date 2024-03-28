02c88049bd39ce3e1608f17d1107972ae2c2ecec
Destyl <zimmerer.max@web.de>
2024-03-28 18:53:36 +0100

    solver exemplary settings

--- src/solver.py
+++ src/solver.py
@@ -11,10 +11,17 @@ if __name__ == "__main__":
     #TODO: get input image
     
     shape = (15, 15)
-    world = World2D(shape, shape, 0.0005, 0.01, 0.01)
-    world.density = np.random.randn(shape[0], shape[1])
+    world = World2D(shape, shape, 0.0005, 0.0, 0.01)
+    # world.density = np.random.randn(shape[0], shape[1])
+    # world.density[5, 5] = 1
+    # world.density[12, 8] = 1
+    world.density[6:7, 2] = 1
     world.velocity[0] = np.random.randn(shape[0], shape[1])
     world.velocity[1] = np.random.randn(shape[0], shape[1])
+    world.velocity[0] = np.zeros(shape)
+    world.velocity[1] = np.zeros(shape)
+    world.velocity[0, 6:8, 1:5] = 10
+    world.velocity[1, 10:12, 2:4] = -5
 
     x = np.linspace(0, shape[0], shape[0])
     y = np.linspace(0, shape[1], shape[1])
@@ -25,7 +32,7 @@ if __name__ == "__main__":
     cmesh = ax2.pcolormesh(X, Y, world.density)
 
     def animate_velocity(num):
-        world.step(0.1)
+        world.step(.1)
 
         quiv.set_UVC(world.velocity[0], world.velocity[1])
         return quiv
@@ -34,8 +41,8 @@ if __name__ == "__main__":
         cmesh.set_array(world.density)
         return cmesh
 
-    anim1 = animation.FuncAnimation(fig, animate_velocity, interval=100, blit=False)
-    anim2 = animation.FuncAnimation(fig, animate_density, interval=100, blit=False)
+    anim1 = animation.FuncAnimation(fig, animate_velocity, interval=1000, blit=False)
+    anim2 = animation.FuncAnimation(fig, animate_density, interval=1000, blit=False)
     plt.show()
 
     # TODO: output image with imageio
